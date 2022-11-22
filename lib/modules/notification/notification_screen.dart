b26474756754ac0a858ef3a372e306d19c8bbd1f
VgionTech <(0$t\U9i9b.{3k*>
2022-11-22 16:06:03 +0530

    various notification types

--- lib/modules/notification/notification_screen.dart
+++ lib/modules/notification/notification_screen.dart
@@ -43,7 +43,61 @@ class _NotificationScreenState extends State<NotificationScreen> {
                 showNormalNotification();
               },
               child: const Text("Show Normal Notification"),
-            )
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showOnGoingNotification();
+              },
+              child: const Text("Show On Going Notification"),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showLargeIconNotification();
+              },
+              child: const Text("Show Large Icon Notification"),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showNotificationWithTimeout();
+              },
+              child: const Text("Show Notification With Timeout"),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showNotificationWithActionButton();
+              },
+              child: const Text("Show Notification With Action Button"),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showNotificationWithTextButton();
+              },
+              child: const Text("Show Notification With Text Button"),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showBigTextNotification();
+              },
+              child: const Text("Show Big Text Notification"),
+            ),
           ],
         ),
       ),
@@ -51,6 +105,32 @@ class _NotificationScreenState extends State<NotificationScreen> {
   }
 
   void showNormalNotification() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        "a", "b",
+        channelDescription: "c",
+        importance: Importance.max,
+        color: Colors.green,
+        priority: Priority.max,
+        enableVibration: true,
+        enableLights: true,
+        ticker: "Ticker...",
+        autoCancel: true,
+        subText: "This is subtext",
+        colorized: true);
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void showOnGoingNotification() async {
     var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
       "a",
       "b",
@@ -60,8 +140,156 @@ class _NotificationScreenState extends State<NotificationScreen> {
       priority: Priority.max,
       enableVibration: true,
       enableLights: true,
-      autoCancel: true,
+      autoCancel: false,
+      ongoing: true,
     );
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'Foreground Notification',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void showLargeIconNotification() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+      "a",
+      "b",
+      channelDescription: "c",
+      importance: Importance.max,
+      color: Colors.green,
+      priority: Priority.max,
+      enableVibration: true,
+      enableLights: true,
+      largeIcon: DrawableResourceAndroidBitmap('@drawable/it_wala'),
+    );
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'Large Icon Notification',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void showNotificationWithTimeout() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        "a", "b",
+        channelDescription: "c",
+        importance: Importance.max,
+        color: Colors.green,
+        priority: Priority.max,
+        enableVibration: true,
+        enableLights: true,
+        timeoutAfter: 5000);
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void showNotificationWithActionButton() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        "a", "b",
+        channelDescription: "c",
+        importance: Importance.max,
+        color: Colors.green,
+        priority: Priority.max,
+        enableVibration: true,
+        enableLights: true,
+        autoCancel: true,
+        subText: "This is subtext",
+        actions: <AndroidNotificationAction>[
+          AndroidNotificationAction(
+            "0",
+            "Action 1",
+            icon: DrawableResourceAndroidBitmap('@drawable/it_wala'),
+          )
+        ]);
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void showNotificationWithTextButton() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        "a", "b",
+        channelDescription: "c",
+        importance: Importance.max,
+        color: Colors.green,
+        priority: Priority.max,
+        enableVibration: true,
+        enableLights: true,
+        autoCancel: true,
+        subText: "This is subtext",
+        actions: <AndroidNotificationAction>[
+          AndroidNotificationAction("1", "Enter Text",
+              icon: DrawableResourceAndroidBitmap('@drawable/it_wala'),
+              inputs: <AndroidNotificationActionInput>[
+                AndroidNotificationActionInput(label: "Enter a message"),
+              ])
+        ]);
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void showBigTextNotification() async {
+    const BigTextStyleInformation bigTextStyleInformation =
+        BigTextStyleInformation(
+      'Lorem <i>ipsum dolor sit</i> amet, consectetur <b>adipiscing elit</b>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      htmlFormatBigText: true,
+      contentTitle: 'overridden <b>big</b> content title',
+      htmlFormatContentTitle: true,
+      summaryText: 'summary <i>text</i>',
+      htmlFormatSummaryText: true,
+    );
+
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        "a", "b",
+        channelDescription: "c",
+        importance: Importance.max,
+        color: Colors.green,
+        priority: Priority.max,
+        enableVibration: true,
+        enableLights: true,
+        ticker: "Ticker...",
+        autoCancel: true,
+        subText: "This is subtext",
+        colorized: true,
+        styleInformation: bigTextStyleInformation);
 
     var platformChannelSpecifics =
         NotificationDetails(android: androidPlatformChannelSpecifics);
