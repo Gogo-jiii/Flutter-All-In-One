d9de7c7e8e685a494c50b1c9e32deef8ad17a05d
VAIBHAV GHANEKAR <woh.it.wala@gmail.com>
2023-02-23 00:04:56 +0530

    current location

--- lib/modules/google_map/google_map_screen.dart
+++ lib/modules/google_map/google_map_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_all_in_one/modules/common_widgets/common_widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 import '../toast/toast_screen.dart';
 
@@ -16,16 +17,20 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  Location? location = Location();
+  late bool _serviceEnabled;
+  late PermissionStatus _permissionGranted;
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  @override
+  void initState() {
+    getLastLocationService();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,20 +58,27 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
         },
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController googleMapController) {
-          _controller.complete(googleMapController);
-        },
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheLake,
-      //   label: const Text('To the lake!'),
-      //   icon: const Icon(Icons.directions_boat),
-      // ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Future<LocationData?> getLastLocationService() async {
+    _serviceEnabled = (await location?.serviceEnabled())!;
+    if (!_serviceEnabled) {
+      _serviceEnabled = (await location?.requestService())!;
+      if (!_serviceEnabled) {
+        return null;
+      }
+    }
+
+    _permissionGranted = (await location?.hasPermission())!;
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = (await location?.requestPermission())!;
+      if (_permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    return await location?.getLocation();
   }
 }
