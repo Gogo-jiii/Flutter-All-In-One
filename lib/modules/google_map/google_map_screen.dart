dd70175f407b0b4ebc3d6ec1d292a95b79a27323
VAIBHAV GHANEKAR <woh.it.wala@gmail.com>
2023-02-23 14:24:53 +0530

    map marker, circle, polygon, etc

--- lib/modules/google_map/google_map_screen.dart
+++ lib/modules/google_map/google_map_screen.dart
@@ -25,10 +25,13 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+  Set<Marker> markers = {};
+  Set<Circle> circles = {};
+  Set<Polygon> polygons = {};
+  List<LatLng> polygonPoints = [];
 
   @override
   void initState() {
-    getLastLocationService();
     super.initState();
   }
 
@@ -37,6 +40,7 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
     return Scaffold(
       appBar: getAppBar(context, "Google Map"),
       body: GoogleMap(
+        onMapCreated: _onMapCreated,
         compassEnabled: true,
         mapToolbarEnabled: true,
         minMaxZoomPreference: MinMaxZoomPreference.unbounded,
@@ -50,11 +54,60 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
         indoorViewEnabled: true,
         trafficEnabled: true,
         buildingsEnabled: true,
-        onTap: (LatLang) {
-          showToast("Clicked. Location: $LatLang");
+        markers: markers,
+        circles: circles,
+        polygons: polygons,
+        onTap: (LatLangValue) {
+          //showToast("Clicked. Location: $LatLangValue");
+          setState(() {
+            markers.add(
+              Marker(
+                draggable: true,
+                position: LatLng(LatLangValue.latitude, LatLangValue.longitude),
+                infoWindow: InfoWindow(
+                  title: "Title",
+                  snippet: "Description",
+                  onTap: () {
+                    showToast("clicked");
+                  },
+                ),
+                markerId: const MarkerId("1"),
+                onDragStart: (LatLangValue) {
+                  showToast("moving...");
+                },
+                onDragEnd: (LatLangValue) {
+                  showToast("Clicked. Location: $LatLangValue");
+                },
+              ),
+            );
+          });
+          setState(() {
+            polygonPoints.add(LatLangValue);
+            polygons.add(
+              Polygon(
+                polygonId: const PolygonId("1"),
+                strokeWidth: 3,
+                strokeColor: Colors.black,
+                fillColor: Colors.blue,
+                points: polygonPoints,
+              ),
+            );
+          });
         },
-        onLongPress: (LatLang) {
-          showToast("Clicked. Location: $LatLang");
+        onLongPress: (LatLangValue) {
+          //showToast("Clicked. Location: $LatLang");
+          setState(() {
+            circles.add(
+              Circle(
+                circleId: const CircleId("1"),
+                radius: 500.0,
+                fillColor: Colors.orange,
+                strokeColor: Colors.white,
+                strokeWidth: 3,
+                center: LatLng(LatLangValue.latitude, LatLangValue.longitude),
+              ),
+            );
+          });
         },
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
@@ -80,5 +133,9 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
     }
 
     return await location?.getLocation();
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    getLastLocationService();
   }
 }
